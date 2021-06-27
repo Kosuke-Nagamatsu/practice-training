73e759d80c0280e14da90a6c33cbe7526ea16ee6
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-06-27 18:55:57 +0900

    feat: タスクの新規作成と編集画面を作成し、設定したバリデーションのエラーメッセージを画面に表示させるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,35 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update]
   def index
     @tasks = Task.all
   end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "新規作成しました！"
+    else
+      render :new
+    end
+  end
   def show
+  end
+  def edit
+  end
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "更新しました！"
+    else
+      render :edit
+    end
+  end
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+  def set_task
     @task = Task.find(params[:id])
   end
 end
