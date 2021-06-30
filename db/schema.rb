384a41f9667d0bc66488c6da9eea9db3d3270e3c
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-06-30 14:28:04 +0900

    feat:タスクに対して終了期限を登録できるようにし、登録した情報を各viewページにて表示するようにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_142049) do
+ActiveRecord::Schema.define(version: 2021_06_30_023444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "content", null: false
+    t.string "title"
+    t.text "content"
+    t.datetime "time_limit", default: -> { "now()" }, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
