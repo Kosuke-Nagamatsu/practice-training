aa25ad0bab8e95a1f2f52b13a61ca2373dfa3690
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-07-01 23:52:48 +0900

    test:終了期限のテストをSystem Specで実装した

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,13 +2,16 @@ FactoryBot.define do
   factory :task do
     title { '1番目に作成したタスクのタイトル' }
     content { '1番目に作成したタスクのコンテント' }
+    time_limit { '2021-07-04 07:15:00' }
   end
   factory :second_task, class: Task do
     title { '2番目に作成したタスクのタイトル' }
     content { '2番目に作成したタスクのコンテント' }
+    time_limit { '2021-07-05 09:30:00' }
   end
   factory :third_task, class: Task do
     title { '最新のタスクのタイトル' }
     content { '最新のタスクの内容' }
+    time_limit { '2021-07-06 010:05:00' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        task = FactoryBot.create(:task, time_limit: '2021-07-07 07:07:00')
+        visit tasks_path
+        expect(page).to have_content('1番目に作成したタスクのタイトル').and have_content('1番目に作成したタスクのコンテント').and have_content('2021年07月07日07時07分')
+      end
+    end
+  end
   describe '一覧表示機能' do
     before do
       FactoryBot.create(:task)
@@ -20,6 +29,13 @@ RSpec.describe 'タスク管理機能', type: :system do
         task_list = all('#task_row td')
         expect(task_list[0]).to have_content '最新のタスクのタイトル'
         expect(task_list[1]).to have_content '最新のタスクの内容'
+      end
+    end
+    context '終了期限でソートするというリンクを押した場合' do
+      it '終了期限の降順に並び替えられたタスク一覧が表示される' do
+        click_link '終了期限でソートする'
+        task_list = all('#task_row td')
+        expect(task_list[2]).to have_content '2021年07月06日10時05分'
       end
     end
   end
