9d0a449865e0d43410665a46513fbbb2d3378b62
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-07-01 22:06:58 +0900

    perf:time_limitカラムにバリデーションを設定し、現在日時かつ過去の日時を終了期限に設定できないようにした

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 40 }
   validates :content, presence: true
+  validate :start_check
+  def start_check
+    errors.add(:time_limit, "は現在の日時より未来の時間を選択してください") if time_limit < Time.now
+  end
 end
