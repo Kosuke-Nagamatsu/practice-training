48c72c49bc391fb7666dcf88c49d9d6f355747e2
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-07-05 23:46:20 +0900

    feat:ログイン機能を実装し、ログインの有無によりapplication.html.erbでの表示を切り替えるコードを実装した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :tasks do
     collection do
