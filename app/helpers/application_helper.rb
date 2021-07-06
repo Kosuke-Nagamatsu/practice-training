36bb5389accbf3dd177a8d7ce603b53cc99bd3f9
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-07-06 21:30:25 +0900

    feat:管理画面でユーザ一覧表示・作成・更新・削除できるようにし、ユーザを削除したらそのユーザに紐づいているタスクを全て削除するようにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
       task_path
     end
   end
+  def choose_new_or_edit_admin
+    if action_name == 'new'
+      admin_users_path
+    elsif action_name == 'edit'
+      admin_user_path
+    end
+  end
 end
