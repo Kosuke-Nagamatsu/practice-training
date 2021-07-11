0875c1a79b25480d72ae60039cfe38f4c63ffe89
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-07-11 21:08:39 +0900

    タスク、ユーザ、ラベルのシードデータを10件ずつ用意した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,25 @@
-labels = ["社外", "社内", "チーム", "個人"]
+10.times do |n|
+  name = Faker::JapaneseMedia::DragonBall.character
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               admin: true
+               )
+end
+10.times do |n|
+  title = Faker::Food.fruits
+  password = "password"
+  Task.create!(title: title,
+               content: "テスト",
+               time_limit: "2021-08-30 10:00:00",
+               status: rand(1..3),
+               priority: rand(1..3),
+               user_id: rand(1..10)
+               )
+end
+labels = ["社外", "社内", "チーム", "個人", "緊急", "高高高", "雑務", "訪問", "出張", "チェック待ち"]
 labels.each do |l|
   Label.create!(name: l)
 end
