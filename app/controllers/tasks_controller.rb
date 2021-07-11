5f51784093b2da9500edc0b8c5f1902061d82941
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-07-11 16:43:56 +0900

    feat:つけたラベルで検索できるようにした（選択した一つのラベルがついているタスクを検索できる。タスク名、ステータスとの複合検索は実装していない）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,18 @@ class TasksController < ApplicationController
         @tasks = current_user.tasks.fuzzy_by_title(params[:task][:title]).full_by_status(params[:task][:status])
       elsif params[:task][:title].present? && params[:task][:status] == '選択してください'
         @tasks = current_user.tasks.fuzzy_by_title(params[:task][:title])
+      elsif params[:task][:label_id].present?
+        # パターン1：
+        # labelsテーブルと内部結合したtasksテーブルのcurrent_userのレコードを取得する
+        # パラメータのidを持つlabelと結合しているレコードを取り出す
+        @tasks = current_user.tasks.joins(:labels).where(labels: { id: params[:task][:label_id] })
+        # パターン2：パラメータのidを持つlabelのレコードを取得し、それに紐づくtaskのレコードを取り出す
+        # @tasks = Label.find(params[:task][:label_id]).tasks
+        # パターン3：
+        # パラメータのidを持つlabelのレコードを取得し、それに紐づくtaskのidを変数ids_taskに代入
+        # current_userのtaskレコードの中で、idがids_taskのレコードを取り出す
+        # ids_task = Label.find(params[:task][:label_id]).task_ids
+        # @tasks = current_user.tasks.where(id: ids_task)
       elsif params[:task][:status] != '選択してください'
         @tasks = current_user.tasks.full_by_status(params[:task][:status])
       end
